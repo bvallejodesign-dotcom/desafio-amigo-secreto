737ac4d0cc82840ccbc6dca88d723d46c9cd0069
Barbara Vallejo <bvallejo.design@gmail.com>
2025-08-19 01:04:53 -0400

    Validar Amigos e ingresar Lista

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
-let nombreAmigo = ''
+let nombreAmigo = '';
 
 //función para agregar amigos
 function agregarAmigo() {
-    nombreAmigo = document.getElementById('amigo').value;
-    console.log(nombreAmigo)
-    return
+    nombreAmigo = (document.getElementById('amigo').value).trim();
+    console.log(nombreAmigo);
 
-    // Validar amigos
+ // Validar amigos
+    if (nombreAmigo == '') {
+        alert('Ingrese un nombre');  
+    } else {
+        amigos.push(nombreAmigo);
+        console.log(amigos);
+    }
+
     
+    
+
 }

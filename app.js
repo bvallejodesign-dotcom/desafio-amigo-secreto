5726e2fddb910215a94845e3e059678fb98f5332
Barbara Vallejo <bvallejo.design@gmail.com>
2025-08-19 17:34:55 -0400

    Crear función para mostrar la lista de amigos en la página

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ function agregarAmigo() {
         console.log(nombreAmigo);
         console.log(amigos);
 
+        //Limpiamos el input con la función de limpieza
         limpiarInput();
+        mostrarListaDeAmigos();
 
     return
 }
@@ -28,4 +30,26 @@ function agregarAmigo() {
 function limpiarInput() {
 
     document.querySelector('#amigo').value = ''
+  
+}
+
+// funcion para mostrar en pantalla a los amigos que agregas a tu lista 
+function mostrarListaDeAmigos() {
+
+    // aqui conecto la lista de HTML con java por medio del ID de HTML
+    let listaDeAmigosHtml = document.getElementById("listaAmigos");
+    //Limpiamos lista
+    listaDeAmigosHtml.innerHTML = '';
+
+    //Recorremos la lista de amigos y creamos los elemetos <li> en html
+    for (let i = 0; i<amigos.length ; i++) {
+
+        //creamos los elementos de la lista
+        let amigoIngresado = document.createElement('li');
+        let texto = document.createTextNode(amigos[i]);
+        amigoIngresado.appendChild(texto);
+
+        //los agregamos a la lista hmtl
+        listaDeAmigosHtml.appendChild(amigoIngresado);
+    }
 }
